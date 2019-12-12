6daf00a029c61411d1a259836dd2e7ebc20d3fe0
Eytan <eytangro@gmail.com>
2019-12-12 13:39:04 +0200

    fixed the minor error we had in algorithmUtils file on the storeAlgorithm function

--- utils/algorithmUtils.js
+++ utils/algorithmUtils.js
@@ -36,7 +36,7 @@ const getAlgorithim = async (name) => {
 }
 
 
-const storeAlgorithm = async (descriptor) => {
+const storeAlgorithm = async (algName) => {
 
     const res = await getAlgorithim(algName)
     write_log(res.status + " " + algName)
@@ -47,9 +47,9 @@ const storeAlgorithm = async (descriptor) => {
 
         const res1 = await chai.request(config.apiServerUrl)
             .post('/store/algorithms/apply')
-            .field('payload', JSON.stringify(descriptor))
-        return res1
+            .field('payload', JSON.stringify(alg))
         logResult(res1, "algorithmUtils storeAlgorithm")
+        return res1
     }
 }
 
