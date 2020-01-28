cfb3720b4c7c09d0df3be638064e94f956b63923
tamir321 <tamir321@gmail.com>
2020-01-28 16:31:14 +0200

    add algorithm_base-version test

---
+++ test/tests/algorithm_base-version.js
@@ -0,0 +1,127 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const path = require('path')
+const delay = require('delay')
+
+const { deleteAlgorithm,
+    runAlgorithm,
+        getAlgorithm,    
+        getAlgorithmVersion,
+        updateAlgorithmVersion,
+        buildAlgoFromImage,
+        deleteAlgorithmVersion
+    } = require(path.join(process.cwd(), 'utils/algorithmUtils'))
+
+
+
+
+
+const {
+    getResult,
+    getRawGraph,
+    getParsedGraph
+  } = require(path.join(process.cwd(), 'utils/results'))
+
+// const KubernetesClient = require('@hkube/kubernetes-client').Client;
+
+
+chai.use(chaiHttp);
+
+describe('baseline  version Tests', () => {
+
+    
+
+    describe('python baseline tests',()=>{   
+        
+        // const python35 = "tamir321/py35"
+        // const python36 = "tamir321/py36"
+        // const python37 = "tamir321/py37"
+        // const python37Slim = "tamir321/py37-slim"
+        const algJson = (algName,imageName) =>{ 
+            let alg = {
+                name: algName,
+                cpu: 1,
+                gpu: 0,
+                mem: "256Mi",
+                minHotWorkers: 0,
+                algorithmImage: imageName,
+                type: "Image",
+                options: {
+                    debug: false,
+                    pending: false
+                    }       
+                }
+            return alg
+        }
+        
+        
+          
+        
+        it('python 2.7', async () => {
+            const algorithmName = "python27"
+            const python27 = "tamir321/py27:01"
+            const algpython27 = algJson(algorithmName,python27)
+           
+            await  deleteAlgorithm(algorithmName,true)
+            await buildAlgoFromImage(algpython27);
+            const alg = {name: algorithmName,
+                            input:[1]}
+            const res = await runAlgorithm(alg)
+            const jobId = res.body.jobId
+            const result = await  getResult(jobId,200)
+             expect(result.data[0].result).to.be.equal(42)
+        }).timeout(1000 * 60 * 5);
+
+
+
+        it('python 3.5', async () => {
+            const algorithmName = "python35"
+            const python35 = "tamir321/py35:01"
+            const algpython35 = algJson(algorithmName,python35)
+           
+            await  deleteAlgorithm(algorithmName,true)
+            await buildAlgoFromImage(algpython35);
+            const alg = {name: algorithmName,
+                            input:[1]}
+            const res = await runAlgorithm(alg)
+            const jobId = res.body.jobId
+            const result = await  getResult(jobId,200)
+             expect(result.data[0].result).to.be.equal(42)
+        }).timeout(1000 * 60 * 5);
+
+        it('python 3.6', async () => {
+            const algorithmName = "python36"
+            const python36 = "tamir321/py36:01"
+            const algpython36 = algJson(algorithmName,python36)
+           
+            await  deleteAlgorithm(algorithmName,true)
+            await buildAlgoFromImage(algpython36);
+            const alg = {name: algorithmName,
+                            input:[1]}
+            const res = await runAlgorithm(alg)
+            const jobId = res.body.jobId
+            const result = await  getResult(jobId,200)
+             expect(result.data[0].result).to.be.equal(42)
+        }).timeout(1000 * 60 * 5);
+
+        it('python 3.7', async () => {
+            const algorithmName = "python37"
+            const python37 = "tamir321/py37:01"
+            const algpython37 = algJson(algorithmName,python37)
+           
+            await  deleteAlgorithm(algorithmName,true)
+            await buildAlgoFromImage(algpython37);
+            const alg = {name: algorithmName,
+                            input:[1]}
+            const res = await runAlgorithm(alg)
+            const jobId = res.body.jobId
+            const result = await  getResult(jobId,200)
+             expect(result.data[0].result).to.be.equal(42)
+        }).timeout(1000 * 60 * 5);
+
+    } )
+    
+   
+
+});
