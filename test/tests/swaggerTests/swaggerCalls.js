db44e1e5263c9d607c0ae6d591f9896b385e8324
tamir321 <48160658+tamir321@users.noreply.github.com>
2020-03-17 13:26:47 +0200

    fix experiment

--- test/tests/swaggerTests/swaggerCalls.js
+++ test/tests/swaggerTests/swaggerCalls.js
@@ -1014,7 +1014,8 @@ describe('all swagger calls test ', () => {
             expect(res).to.have.status(200)
             expect(res.body.name).to.be.equal(name)
           
-        })
+        }).timeout(1000 * 60 * 2)
+
 
         it('test  POST Delete /experiment', async () => {
            
@@ -1033,18 +1034,19 @@ describe('all swagger calls test ', () => {
             expect(resGet).to.have.status(404)
 
           
-        })
+        }).timeout(1000 * 60 * 2)
+
 
 
 
         it('test /experiment​/list​/all', async () => {
           
             const res = await chai.request(config.apiServerUrl)
-                .get(`​/experiment​/list​/all`)
+            .get(`/experiment/list/all`)
 
             expect(res).to.have.status(200)         
           
-        })
+        }).timeout(1000 * 60 * 2)
 
 
 
