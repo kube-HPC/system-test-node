5a2b532b2133c5a60309c11251d30408b4984444
tamir321 <tamir321@gmail.com>
2020-05-13 09:48:10 +0300

    fixes

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@ echo Deploy to kubernetes
 source ./setPath
 helm repo add hkube-dev http://hkube.io/helm/dev/
 helm repo update
-envsubst < ./values-pub-template.yml > ~/pub.yml
+envsubst < ./travis/values-pub-template.yml > ~/pub.yml
 helm upgrade --wait --timeout 600 -i hkube -f ~/pub.yml hkube-dev/hkube
 helm ls
 
--- travis/install.sh
+++ travis/install.sh
@@ -7,7 +7,7 @@ echo "export PATH=$PWD/bin:$PATH" > setPath
 source ./setPath
 mv ./kubectl $PWD/bin/kubectl
 mkdir -p ~/.kube/
-envsubst < ./kube-config-template.yml > ~/.kube/config
+envsubst < ./travis/kube-config-template.yml > ~/.kube/config
 kubectl cluster-info
 curl -LO https://storage.googleapis.com/kubernetes-helm/helm-v2.14.3-linux-amd64.tar.gz
 tar -zxvf helm-v2.14.3-linux-amd64.tar.gz
