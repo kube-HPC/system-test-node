d40719a7907a1737385ccf5f1898955e6a048f85
tamir reiss <tamir321@gmail.com>
2020-07-26 15:38:53 +0300

    remove timeout

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,6 +4,6 @@ source ./setPath
 helm repo add hkube-dev http://hkube.io/helm/dev/
 helm repo update
 envsubst < ./travis/values-pub-template.yml > ~/pub.yml
-helm upgrade --wait --timeout 600 -i hkube -f ~/pub.yml hkube-dev/hkube
+helm upgrade -i hkube -f ~/pub.yml hkube-dev/hkube
 helm ls
 
