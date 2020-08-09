ea8996ad90ac1e31e52a4cbd86131f4ae4904081
tamir reiss <tamir321@gmail.com>
2020-08-09 16:59:51 +0300

    add .kube to HightAvailability sh

--- travis/HighAvailability.sh
+++ travis/HighAvailability.sh
@@ -1,7 +1,11 @@
 echo Running tests
+mkdir -p ~/.kube/
+envsubst < ./travis/kube-config-template.yml > ~/.kube/config
+export K8S_CONFIG_PATH=~/.kube/config
 export BASE_URL=https://${KUBERNETES_MASTER_IP}
 export WEBHOOK_URL=${WEBHOOK_MASTER_URL}
 export K8S_VERSION=1.13
+
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo ${K8S_VERSION}
 echo ${BASE_URL}
