6b6f23f062bce839d86cb36ccee53d5ea8fbf432
tamir reiss <tamir321@gmail.com>
2020-08-09 16:32:16 +0300

    clean HighAvailability sh  file

--- travis/HighAvailability.sh
+++ travis/HighAvailability.sh
@@ -1,35 +1,9 @@
 echo Running tests
-echo start download hkubectl
 export BASE_URL=https://${KUBERNETES_MASTER_IP}
 export WEBHOOK_URL=${WEBHOOK_MASTER_URL}
 export K8S_VERSION=1.13
-curl -Lo hkubectl https://github.com/kube-HPC/hkubectl/releases/download/$(curl -s https://api.github.com/repos/kube-HPC/hkubectl/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/hkubectl-linux \
-&& chmod +x hkubectl 
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo path before 
-echo $PATH
-export PWD = `/bin/pwd`
-export PATH=${PWD}:${PATH}
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo path After 
-echo $PATH
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mkdir -p ~/.hkube
-cat <<EOF >~/.hkube/.hkuberc
-{
-  "endpoint": "https://test.hkube.io",
-  "rejectUnauthorized": false
-}
-EOF
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo ls ~/.hkube -a
-ls ~/.hkube -a
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo which hkubectl
-which hkubectl
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo  hkubectl --help
-hkubectl --help
-#&& sudo mv hkubectl /usr/local/bin/
+echo ${K8S_VERSION}
+echo ${BASE_URL}
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 npm run HighAvailability
