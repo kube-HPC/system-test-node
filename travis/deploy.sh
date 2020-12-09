c8a6ee6b87f7449b7f735a63b6a99dce696571b1
Yehiyam Livneh <yehiyam@gmail.com>
2020-12-09 15:42:52 +0200

    ci

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Deploy to kubernetes
-source ./setPath
 helm repo add hkube-dev http://hkube.io/helm/dev/
 helm repo update
-envsubst < ./travis/values-pub-template.yml > ~/pub.yml
-helm upgrade -i hkube -f ~/pub.yml hkube-dev/hkube
-helm ls
+envsubst < ./travis/values-pub-template.yml > /tmp/pub.yml
+helm search repo hkube
+helm upgrade -i hkube -f /tmp/pub.yml hkube-dev/hkube
+helm ls --all
 
