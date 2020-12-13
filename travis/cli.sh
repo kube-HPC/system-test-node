ac77a324c6a6850b6b8083882741b98747e96943
tamir reiss <tamir321@gmail.com>
2020-12-13 12:20:34 +0200

    1

--- travis/cli.sh
+++ travis/cli.sh
@@ -3,14 +3,14 @@ echo start download hkubectl
 
 curl -Lo hkubectl https://github.com/kube-HPC/hkubectl/releases/download/$(curl -s https://api.github.com/repos/kube-HPC/hkubectl/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/hkubectl-linux \
 && chmod +x hkubectl 
-#echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#echo path before 
-#echo $PATH
-#export PWD=`/bin/pwd`
-#export PATH=${PWD}:${PATH}
-#echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#echo path After 
-#echo $PATH
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo path before 
+echo $PATH
+export PWD=`/bin/pwd`
+export PATH=${PWD}:${PATH}
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo path After 
+echo $PATH
 echo ~~~~~~~~~~~~~~~~~~~~~~mkdir -p ~/.hkube~~~~~~~~~~~~~~~~~~~~~~~~~
 mkdir -p ~/.hkube
 cat <<EOF >~/.hkube/.hkuberc
