660909a04ff243440ae3b311cf28aacf4714d2df
tamir reiss <tamir321@gmail.com>
2020-12-13 12:15:13 +0200

    try cli

--- travis/cli.sh
+++ travis/cli.sh
@@ -3,15 +3,15 @@ echo start download hkubectl
 
 curl -Lo hkubectl https://github.com/kube-HPC/hkubectl/releases/download/$(curl -s https://api.github.com/repos/kube-HPC/hkubectl/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/hkubectl-linux \
 && chmod +x hkubectl 
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo path before 
-echo $PATH
-export PWD = `/bin/pwd`
-export PATH=${PWD}:${PATH}
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo path After 
-echo $PATH
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#echo path before 
+#echo $PATH
+#export PWD=`/bin/pwd`
+#export PATH=${PWD}:${PATH}
+#echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#echo path After 
+#echo $PATH
+echo ~~~~~~~~~~~~~~~~~~~~~~mkdir -p ~/.hkube~~~~~~~~~~~~~~~~~~~~~~~~~
 mkdir -p ~/.hkube
 cat <<EOF >~/.hkube/.hkuberc
 {
@@ -19,17 +19,19 @@ cat <<EOF >~/.hkube/.hkuberc
   "rejectUnauthorized": false
 }
 EOF
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~ls ~/.hkube -a~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo ls ~/.hkube -a
 ls ~/.hkube -a
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~which hkubectl~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo which hkubectl
 which hkubectl
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~hkubectl --help~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo  hkubectl --help
 hkubectl --help
 #&& sudo mv hkubectl /usr/local/bin/
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~algorithm get green-alg~~~~~~~~~~~~~~~~~~~~~~~
+hkubectl algorithm get green-alg --json
+echo ~~~========================================================~~~~~
 #export BASE_URL=https://${KUBERNETES_MASTER_IP}
 #export WEBHOOK_URL=${WEBHOOK_MASTER_URL}
 #npm run cliTests
