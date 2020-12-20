ed4c919eb31ff049cb14af6d827e7f6afadcc5d4
tamir reiss <tamir321@gmail.com>
2020-12-20 12:14:50 +0200

    fix mac cli

---
+++ travis/cliMac.sh
@@ -0,0 +1,18 @@
+echo Running tests
+echo start download hkubectl
+
+curl -Lo hkubectl https://github.com/kube-HPC/hkubectl/releases/download/$(curl -s https://api.github.com/repos/kube-HPC/hkubectl/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/hkubectl-macos \
+&& chmod +x hkubectl 
+
+mkdir -p ~/.hkube
+cat <<EOF >~/.hkube/.hkuberc
+{
+  "endpoint": "${HKUBE_URL}",
+  "rejectUnauthorized": false
+}
+EOF
+
+echo ~~~~~~~~~~~~~algorithm get green-alg~~~~~~~~~~~~~~~~~~~~~~~
+./hkubectl --version
+./hkubectl algorithm get green-alg --json
+echo ~~~========================================================~~~~~
