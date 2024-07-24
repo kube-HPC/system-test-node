91f3d6a007223a0688a134302da47029c25d0e7e
Adir111 <adir987321@gmail.com>
2024-07-24 17:34:15 +0300

    added a new query to get all algorithms

---
+++ utils/graphql/queries/all-algorithms.js
@@ -0,0 +1,61 @@
+const { gql } = require('graphql-request')
+
+const ALL_ALGORITHMS_QUERY = gql`
+query Algorithms {
+  algorithms {
+    list {
+      name
+      isSatisfied
+      cpu
+      created
+      entryPoint
+      env
+      gpu
+      mem
+      minHotWorkers
+      modified
+      reservedMemory
+      type
+      algorithmImage
+      version
+      debugUrl
+      errors
+      options {
+        debug
+        pending
+        batchTolerance
+        progressVerbosityLevel
+        ttl
+        concurrentPipelines {
+          amount
+          rejectOnFailure
+        }
+      }
+      gitRepository {
+        gitKind
+        url
+        branchName
+        webUrl
+        cloneUrl
+        commit {
+          id
+          timestamp
+          message
+        }
+      }
+      buildStats {
+        total
+        pending
+        creating
+        active
+        completed
+        failed
+        stopped
+      }
+    }
+    algorithmsCount
+  }
+}
+`;
+
+module.exports = ALL_ALGORITHMS_QUERY;
