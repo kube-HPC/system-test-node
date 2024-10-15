37b9c827f259009137db032273bfa6ab534dc5ec
Adir111 <adir987321@gmail.com>
2024-10-15 15:20:41 +0300

    added fancy delay

--- utils/misc_utils.js
+++ utils/misc_utils.js
@@ -12,8 +12,27 @@ const write_log = (st, sv = 'info') => {
 
 }
 
+/**
+ * A delay function that prints a progress message at each interval,
+ * updating the same log line with each check.
+ *
+ * @param {string} msg - The message to display during the delay.
+ * @param {number} timeout - The total timeout in milliseconds for the delay.
+ * @param {number} [interval=10000] - Optional. The delay interval in milliseconds between each check.
+ * @returns {Promise<void>} - A promise that resolves after the specified timeout.
+ */
+const intervalDelay = async (msg, timeout, interval = 10 * 1000) => {
+    const totalChecks = Math.ceil(timeout / interval);
+    for (let check = 1; check <= totalChecks; check++) {
+        process.stdout.write(`\r${msg}: ${check}/${totalChecks}`);
+        await delay(interval);
+    }
+    console.log();
+};
+
 
 
 module.exports = {
-    write_log
+    write_log,
+    intervalDelay
 }
