512fc160aefa4e32c742607a601a14ef4138b82d
Adir111 <adir987321@gmail.com>
2024-10-30 10:07:41 +0200

    fixed image

--- additionalFiles/defaults/algorithms/timeStartstream.js
+++ additionalFiles/defaults/algorithms/timeStartstream.js
@@ -9,7 +9,7 @@ const alg = {
   "cpu": 0.1,
   "mem": "128Mi",
   "minHotWorkers": 0,
-  "algorithmImage": "http://docker.io/hkubedevtest/start-streaming:vzu3ucyu6",
+  "algorithmImage": "docker.io/hkubedevtest/start-streaming:vzu3ucyu6",
   "workerEnv": {
     "INACTIVE_WORKER_TIMEOUT_MS": 1000,
     "AUTO_SCALER_MAX_REPLICAS_PER_SCALE": 100
