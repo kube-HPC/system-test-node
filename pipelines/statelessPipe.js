944605ec6bf53194c7e9f10a2988a52ec9e957d4
golanha <g.olanlia@gmail.com>
2024-11-19 09:46:06 +0200

    use test stayup

--- pipelines/statelessPipe.js
+++ pipelines/statelessPipe.js
@@ -1,77 +1,77 @@
-const descriptor = 
+const descriptor =
 {
-    name: "statelessPipe",
-    kind: "stream",
-    flowInput: {
-      msgPerSec: 80,
-      delay: 0.03,
-      rate: 100
+  name: "statelessPipe",
+  kind: "stream",
+  flowInput: {
+    msgPerSec: 80,
+    delay: 0.03,
+    rate: 100
+  },
+  streaming: {
+    flows: {
+      analyze: "A >> B"
     },
-    streaming: {
-      flows: {
-        analyze: "A >> B"
-      },
-      defaultFlow: "analyze"
+    defaultFlow: "analyze"
+  },
+  options: {
+    batchTolerance: 80,
+    concurrentPipelines: {
+      amount: 10,
+      rejectOnFailure: true
     },
-    options: {
-      batchTolerance: 80,
-      concurrentPipelines: {
-        amount: 10,
-        rejectOnFailure: true
+    ttl: 3600,
+    progressVerbosityLevel: "info"
+  },
+  priority: 3,
+  nodes: [
+    {
+      kind: "algorithm",
+      stateType: "stateful",
+      nodeName: "A",
+      algorithmName: "stayuptestalg",
+      input: [
+        {
+          totalMsg: 1000000,
+          rng: 10,
+          burst: 2,
+          burstDuration: 30,
+          burstTime: 160,
+          sleepTime: [
+            7,
+            60
+          ],
+          error: false,
+          size: 1,
+          ping: 30
+        }
+      ],
+      retry: {
+        policy: "OnCrash",
+        limit: 3
       },
-      ttl: 3600,
-      progressVerbosityLevel: "info"
+      ttl: 0
     },
-    priority: 3,
-    nodes: [
-      {
-        kind: "algorithm",
-        stateType: "stateful",
-        nodeName: "A",
-        algorithmName: "stayup",
-        input: [
-          {
-            totalMsg: 1000000,
-            rng: 10,
-            burst: 2,
-            burstDuration: 30,
-            burstTime: 160,
-            sleepTime: [
-              7,
-              60
-            ],
-            error: false,
-            size: 1,
-            ping: 30
-          }
-        ],
-        retry: {
-          policy: "OnCrash",
-          limit: 3
-        },
-        ttl: 0
+    {
+      kind: "algorithm",
+      stateType: "stateless",
+      nodeName: "B",
+      algorithmName: "yellow-alg",
+      minStatelessCount: 3,
+      input: [],
+      retry: {
+        policy: "OnCrash",
+        limit: 3
       },
-      {
-        kind: "algorithm",
-        stateType: "stateless",
-        nodeName: "B",
-        algorithmName: "yellow-alg",
-        minStatelessCount: 3,
-        input: [],
-        retry: {
-          policy: "OnCrash",
-          limit: 3
-        },
-        ttl: 0
-      }
-    ]
-  }
-
-
-  const input = {
-    flowInput: {
-        
+      ttl: 0
     }
- }
+  ]
+}
 
-  module.exports = { descriptor, input }
+
+const input = {
+  flowInput: {
+
+  }
+}
+
+module.exports = { descriptor, input }
