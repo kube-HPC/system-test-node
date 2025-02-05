b59c8b9a202a3a1cb723c9229d67983a81ed0d97
Adir111 <adir987321@gmail.com>
2025-02-05 18:52:51 +0200

    Organised and made better logging for method failure.

--- utils/jaeger.js
+++ utils/jaeger.js
@@ -7,9 +7,19 @@ const logger = require('../utils/logger')
 
 const getSpansByJodid = async (jobId) => {
     const res = await chai.request(config.baseUrl).
-        get(`/hkube/api-server//api/v1/jaeger?jobId=${jobId}`)
+        get(`/hkube/api-server//api/v1/jaeger?jobId=${jobId}`);
 
-    return JSON.parse(res.text).data[0].spans
+    if (res.status !== 200) {
+        throw new Error(`Failed to get spans: Code ${res.status}\n${res.text}`);
+    }
+
+    const responseBody = JSON.parse(res.text);
+
+    if (!responseBody.data || !responseBody.data[0] || !responseBody.data[0].spans) {
+        throw new Error(`No spans found for jobId: ${jobId}`);
+    }
+
+    return JSON.parse(res.text).data[0].spans;
 }
 //  https://cicd-test.hkube.org/hkube/api-server//api/v1/jaeger?jobId=main:versatile-pipe:c005ca0a-d318-420f-94c3-63e566b9a126
 module.exports = { getSpansByJodid }
