3cf1b9d7db03347aa16b478b53d8638dfcc70f0a
Adir111 <adir987321@gmail.com>
2025-02-10 10:25:11 +0200

    Organised

--- utils/webhook.js
+++ utils/webhook.js
@@ -1,23 +1,21 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expect = chai.expect;
 
 chai.use(chaiHttp);
-const path = require('path')
-const config = require(path.join(process.cwd(), 'config/config'))
+const path = require('path');
+const config = require(path.join(process.cwd(), 'config/config'));
 
 const getProgress = async (jobId) => {
     const res = await chai.request(config.webhookUrl)
-        .get(`/progress/${jobId}`)    
-    return res
+        .get(`/progress/${jobId}`);   
+    return res;
 }
 
 const getResults = async (jobId) => {
     const res = await chai.request(config.webhookUrl)
-        .get(`/results/${jobId}`)    
-    return res
+        .get(`/results/${jobId}`);  
+    return res;
 }
-
 
 module.exports = { 
     getProgress,
