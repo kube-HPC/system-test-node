f5e5052b5dd38372c892de4b7088f70d8d4f4a8a
Adir111 <adir987321@gmail.com>
2025-02-10 11:18:53 +0200

    organised

--- tests/processEvent.js
+++ tests/processEvent.js
@@ -1,19 +1,22 @@
-
 process.on('exit', (code) => {
     console.log(`exit code ${code}`);
 });
+
 process.on('SIGINT', () => {
     console.log('SIGINT');
     process.exit(0);
 });
+
 process.on('SIGTERM', () => {
     console.log('SIGTERM');
     process.exit(0);
 });
+
 process.on('unhandledRejection', (error) => {
     console.log(`unhandledRejection: ${error.message}`, error);
     process.exit(1);
 });
+
 process.on('uncaughtException', (error) => {
     console.log(`uncaughtException: ${error.message}`, error);
     process.exit(1);
