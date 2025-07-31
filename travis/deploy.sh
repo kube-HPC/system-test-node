3dc527206c5b489b5ee12793dc452bc07d822111
Adir David <adir987321@gmail.com>
2025-07-31 11:12:15 +0300

    setting manually tag to v2.9.4

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -7,6 +7,7 @@ helm repo add hkube-dev http://"$domain"/helm/dev/
 helm repo update
 envsubst < ./travis/values-pub-template.yml > /tmp/pub.yml
 helm search repo hkube
-helm upgrade -i hkube -f /tmp/pub.yml hkube-dev/hkube
+# helm upgrade -i hkube -f /tmp/pub.yml hkube-dev/hkube
+helm upgrade -i hkube -f /tmp/pub.yml --set datasources_service.image.tag=v2.9.4 hkube-dev/hkube
 helm ls --all
 echo end Of Script
