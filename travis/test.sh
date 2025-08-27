fb1fea6f016a438cc70acf2e87c7852afc6d1f78
Adir David <adir987321@gmail.com>
2025-08-27 19:16:32 +0300

    testing

--- travis/test.sh
+++ travis/test.sh
@@ -1,4 +1,9 @@
 echo Running tests
 export BASE_URL=https://${KUBERNETES_MASTER_IP}
 export WEBHOOK_URL=${WEBHOOK_MASTER_URL}
+
+echo "BASE_URL=$BASE_URL"
+nslookup $(echo $BASE_URL | sed 's~https\?://~~')
+curl -vk $BASE_URL/auth/login || true
+
 npm test
