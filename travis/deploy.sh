c4f847bacc96f0ae5a3573c0b563aaff0209b2c4
Adir David <adir987321@gmail.com>
2025-09-18 15:58:37 +0300

    restored

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -7,6 +7,6 @@ helm repo add hkube-dev http://"$domain"/helm/dev/
 helm repo update
 envsubst < ./travis/values-pub-template.yml > /tmp/pub.yml
 helm search repo hkube
-helm upgrade -i hkube -f /tmp/pub.yml hkube-dev/hkube --atomic
+helm upgrade -i hkube -f /tmp/pub.yml hkube-dev/hkube
 helm ls --all
 echo end Of Script
