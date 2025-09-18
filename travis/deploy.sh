f6a3c2be8286f544e6abb28a41445e2e373cf73f
Adir David <adir987321@gmail.com>
2025-09-18 15:55:47 +0300

    should fail workflow if helm upgrade fails!

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -7,9 +7,6 @@ helm repo add hkube-dev http://"$domain"/helm/dev/
 helm repo update
 envsubst < ./travis/values-pub-template.yml > /tmp/pub.yml
 helm search repo hkube
-helm upgrade -i hkube -f /tmp/pub.yml hkube-dev/hkube || {
-  echo "Helm deployment failed!"
-  exit 1
-}
+helm upgrade -i hkube -f /tmp/pub.yml hkube-dev/hkube --atomic
 helm ls --all
 echo end Of Script
